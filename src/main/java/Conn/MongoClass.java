7937568cc65c90f3e4c43e86186d53cceb1b6e84
PPAULCuentaGitHub <cuentamacppaul@gmail.com>
2021-01-14 23:43:23 -0500

    Commit_4

--- src/main/java/Conn/MongoClass.java
+++ src/main/java/Conn/MongoClass.java
@@ -8,21 +8,54 @@ import com.mongodb.Mongo;
 
 import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+
+
+
 
 public class MongoClass {
 
 	public static void main(String[] args) {
 		
+		
 		try {
-			MongoClient mongoClient = new MongoClient("localhost",27017);
-			System.out.println("CONEXION EXITOSA");
-		} catch ( Exception e ) {
-			// TODO: handle exception
 			
+			MongoClient mongo= new MongoClient();
+			DB db= mongo.getDB("OCTAVO");
+			
+			
+			DBCollection coleccion= db.getCollection("notaDeCreditoVentas");
+			BasicDBObject objeto= new BasicDBObject();
+			
+			notaDeCreditoVentas ncv = new notaDeCreditoVentas ();
+			
+			
+			objeto.put("fechaNotaDeCreditoVentas",ncv.getFechaNotaDeCreditoVentas());
+			objeto.put("idDocumentoDeVentasVentas",ncv.getIdDocumentoDeVentasVentas());
+			objeto.put("responsableNotaDeCreditoVentas",ncv.getResponsableNotaDeCreditoVentas());
+			objeto.put("observacionesNotaDeCreditoVentas",ncv.getObservacionesNotaDeCreditoVentas());
+			objeto.put("tipoNotaDeCreditoVentas",ncv.getTipoNotaDeCreditoVentas());			
+			coleccion.insert(objeto);
+			
+			
+			/*
+			
+			
+			DBCollection coleccion= db.getCollection("Persona");
+			BasicDBObject objeto= new BasicDBObject();
+			Persona p= new Persona("pepe","perez");
+			objeto.put("nombre",p.getNombre() );
+			objeto.put("apellidos",p.getApellidos());
+			coleccion.insert(objeto);
+			*/
+			
+			
+			
+			} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
-		}
+			}
 
 	}
 
---
+++ src/main/java/Conn/notaDeCreditoVentas.java
@@ -0,0 +1,53 @@
+package Conn;
+
+public class notaDeCreditoVentas {
+
+	public notaDeCreditoVentas() {
+		
+	}
+	
+	String fechaNotaDeCreditoVentas, idDocumentoDeVentasVentas,responsableNotaDeCreditoVentas,observacionesNotaDeCreditoVentas,tipoNotaDeCreditoVentas;
+
+	public String getFechaNotaDeCreditoVentas() {
+		return fechaNotaDeCreditoVentas;
+	}
+
+	public void setFechaNotaDeCreditoVentas(String fechaNotaDeCreditoVentas) {
+		this.fechaNotaDeCreditoVentas = fechaNotaDeCreditoVentas;
+	}
+
+	public String getIdDocumentoDeVentasVentas() {
+		return idDocumentoDeVentasVentas;
+	}
+
+	public void setIdDocumentoDeVentasVentas(String idDocumentoDeVentasVentas) {
+		this.idDocumentoDeVentasVentas = idDocumentoDeVentasVentas;
+	}
+
+	public String getResponsableNotaDeCreditoVentas() {
+		return responsableNotaDeCreditoVentas;
+	}
+
+	public void setResponsableNotaDeCreditoVentas(String responsableNotaDeCreditoVentas) {
+		this.responsableNotaDeCreditoVentas = responsableNotaDeCreditoVentas;
+	}
+
+	public String getObservacionesNotaDeCreditoVentas() {
+		return observacionesNotaDeCreditoVentas;
+	}
+
+	public void setObservacionesNotaDeCreditoVentas(String observacionesNotaDeCreditoVentas) {
+		this.observacionesNotaDeCreditoVentas = observacionesNotaDeCreditoVentas;
+	}
+
+	public String getTipoNotaDeCreditoVentas() {
+		return tipoNotaDeCreditoVentas;
+	}
+
+	public void setTipoNotaDeCreditoVentas(String tipoNotaDeCreditoVentas) {
+		this.tipoNotaDeCreditoVentas = tipoNotaDeCreditoVentas;
+	}
+	
+	
+
+}
