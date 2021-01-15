a38f2e7d8913262db2635e3d31aa9b12fa0340df
PPAULCuentaGitHub <cuentamacppaul@gmail.com>
2021-01-14 21:30:18 -0500

    Inicio del proyecto

---
+++ src/main/java/com/octavo/api2/octavoproyectoapi2/Octavoproyectoapi2Application.java
@@ -0,0 +1,13 @@
+package com.octavo.api2.octavoproyectoapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Octavoproyectoapi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Octavoproyectoapi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/octavo/api2/octavoproyectoapi2/Octavoproyectoapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octavo.api2.octavoproyectoapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Octavoproyectoapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
